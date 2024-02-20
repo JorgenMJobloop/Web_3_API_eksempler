883b2e9eb1552897457c60bf11dc992d40ea3df9
JorgenMJobloop <jorgen@jobloop.no>
2024-02-20 12:51:06 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const imgUrl = document.getElementById("img-url");
 const pokemonDescription = document.getElementById("pokemons");
 const myText = document.getElementById("my-text");
 
+let myObject = {
+    1: "Hello",
+    2: "World",
+    3: ["Hello", "World"]
+};
+
+let myArray = ["Hello", "World", "From", "My", "Array"];
+
 async function getData() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon");
     const data = await response.json();
@@ -13,4 +21,15 @@ async function getData() {
 
 dataButton.addEventListener("click", getData);
 
-myText.textContent = "";
+// vanlig for-loop
+
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(i);
+//     console.log(myArray[i]);
+// }
+
+// filter method som gjør det samme som en index-basert for-loop
+myArray.filter((arr, i, newArr) => (console.log(arr, i, newArr.push("Array 1"), newArr.push("Array 2"), newArr)));
+// myArray.reverse();
+
+console.log(myObject[3]);
